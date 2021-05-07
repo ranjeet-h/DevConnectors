5c839ba3ab39f0a5a86bbe9d8992446c16b0bc0b
Ranjeet Harishchandre <ranjeetharishchandre@gmail.com>
2021-05-07 22:00:51 +0530

    Started Front end (Redux) Chapter - 7 Leasson - 1

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,26 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-      <nav className="navbar bg-dark">
-        <h1>
-          <a href="index.html">
-            <i className="fas fa-code"></i> DevConnector
-          </a>
-        </h1>
-        <ul>
-          <li>
-            <a href="profiles.html">Developers</a>
-          </li>
-          <li>
-            <a href="register.html">Register</a>
-          </li>
-          <li>
-            <a href="login.html">Login</a>
-          </li>
-        </ul>
-      </nav>
-    );
-}
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <Link to="/">
+          <i className="fas fa-code"></i> DevConnector
+        </Link>
+      </h1>
+      <ul>
+        <li>
+          <Link to="!#">Developers</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
